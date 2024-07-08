4010e8f7f0d49b90d1b61b7d41d7e041ab2c8a19
Eto <L2552704574@outlook.com>
2024-07-08 21:41:08 +0800

    NO.1的第一次修改

--- NO.1 Sum_of_two_numbers.cpp
+++ NO.1 Sum_of_two_numbers.cpp
@@ -1,62 +1,54 @@
-#include<iostream>
+#include<iostream>  
+#include<vector>  
 using namespace std;
-#include<vector>
 
-vector<int>twoSum(vector<int> nums, int target) {
-	int n = nums.size();
-	vector<int>num(3, 1);
-	for (int i = 0; i < n; ++i) {
-		for (int j = i + 1; j < n; ++j) {
-			if (nums[i] + nums[j] == target) {
-				nums.clear();
-				nums.push_back(i);
-				nums.push_back(j);
-				nums.push_back(1);
-				return nums;
-			}
-		}
-	}
-	return nums;
+vector<int> twoSum(vector<int>& nums, int target) {
+    int n = nums.size();
+    vector<int> result;
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            if (nums[i] + nums[j] == target) {
+                result.push_back(i);
+                result.push_back(j);
+                return result;
+            }
+        }
+    }
+    return result; // 如果没有找到，返回一个空的vector  
 }
 
-int main()
-{
+int main() {
+    int n;
+    cout << "Please input the length of the array arr: ";
+    cin >> n;
+    int* arr = new int[n];
+    cout << "Please input the numbers:";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    vector<int> nums;
 
-	int n;
-	cout << "Please input the length of the array arr: ";
-	cin>>n;
-	int* arr = new int[n];
-	cout << "Please input the numbers:";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
-	}
-	vector<int> nums;
+    cout << "arr[" << n << "]=";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 
-	cout << "arr[" << n << "]=";
-	for (int i = 0; i < n; i++) 
-	{
-		cout << arr[i] << " ";
-	}
-	cout << endl;
+    for (int i = 0; i < n; i++) {
+        nums.push_back(arr[i]);
+    }
 
-	for (int i = 0; i < n; i++)
-	{
-		nums.push_back(arr[i]);
-	}
+    int target;
+    cout << "Please input the target:";
+    cin >> target;
+    vector<int> res = twoSum(nums, target);
+    if (!res.empty()) {
+        cout << "that is " << res[0] << " plus " << res[1] << " equals " << target;
+    }
+    else {
+        cout << "There are no two numbers in the array arr that add up to " << target << endl;
+    }
 
-	int target = 0;
-	cout << "Please input the target:";
-	cin >> target;
-	vector<int>res = twoSum(nums, target);
-	switch (res[3])
-	{
-		case 1:
-			cout << "that is " << res[0] << " plus " << res[1] << " equals " << target;
-			break;
-		default:
-			cout << "There are no two numbers in the array arr that add up to the "<< target << endl;
-	}
-	
-	delete[] arr;
+    delete[] arr;
+    return 0;
 }
